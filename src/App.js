0e1d99a2f5532244883a932d52c345154a3796a4
Rémy Jacob <rjpro77@outlook.fr>
2019-04-01 10:55:14 +0200

    add all file

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Quote from './Componemts/Quote';
+import Quotes from './Componemts/Quotes'
+import Lamp from './Componemts/Lamp'
+
+
+
+
+class App extends Component {
+
+  state = {
+    working: true
+  }
+
+handleClick = () => {
+  this.setState({working: !this.state.working})
+};
+  render() {
+    const logoSpin = this.state.working ? 'App-logo' : 'App-logo-fast'
+    const buttonState = this.state.working ? 'il fait trop chaud pour travailler' : 'allez je suis chaud patate'
+    const homerState = this.state.working ? 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDSS0XoTTotYR8KL9Xpa8iEV8g1ouMlmQM-av_strKiMF5McAK' :'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCxJLgwWg1R-FO6PeSCo-OqhFQpwgpixklYOthG749gsEOHUqq'
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo}  className= {logoSpin} alt="logo"/>
+          
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+         
+        </header>
+        <button onClick = { this.handleClick} 
+            >{buttonState}
+        </button>
+        <div className = 'homer'>
+        <img src={homerState} alt="homer at work"/> 
+        </div> 
+        <Lamp />
+        <Lamp />
+        <Quote  
+          quote="I believe the children are the future... Unless we stop them now!"
+          character="Homer Simpson"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
+           />
+        <Quote
+          quote="Me fail English? That's unpossible"
+          character="Ralph Wiggum"
+          image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
+        />
+        <Quotes/>   
+      </div>
+    );
+  }
+}
+
+
+export default App;
