b5b296052b853f55db86353fc136913a118e0fc7
Rémy Jacob <rjpro77@outlook.fr>
2019-04-08 11:27:14 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -7,24 +7,18 @@ import Lamp from './Componemts/Lamp'
 import DisplayQuotes from './Componemts/DisplayQuotes'
 import GenerateQuotes from './Componemts/GenerateQuotes'
 
-const ex =
-{
-  quote: "Le vol à l'étalage est un crime sans victime, c'est comme frapper quelqu'un dans le noir.",
-  character: "Nelson Muntz",
-  image: "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FNelsonMuntz.png?1497567511185",
-  characterDirection: "Left"
-}
+
 class App extends Component {
 
   state = {
     working: true,
-    person: ex
+    person: {}
 
   }
   getQuotes() {
     fetch("https://thesimpsonsquoteapi.glitch.me/quotes")
       .then(response => response.json())
-      .then(data =>{ console.log(data)
+      .then(data =>{ 
        this.setState({
          person: data[0],
         })
@@ -35,7 +29,9 @@ class App extends Component {
   }
   
 
-  
+  componentDidMount(){
+    this.getQuotes()
+  }
 
 
   render() {
